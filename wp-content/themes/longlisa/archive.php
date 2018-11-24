9e636cfcce0bb203e4ba2f28be8df6b7e3ceea28
Lisa Long <garrisonridge@gmail.com>
2018-11-24 17:23:09 -0500

    Create archive.php + tag.php

--- wp-content/themes/longlisa/archive.php
+++ wp-content/themes/longlisa/archive.php
@@ -1,19 +1,85 @@
 <?php get_header(); ?>
 
-	<main role="main">
-		<!-- section -->
-		<section>
+<!-- BILL BOARD -->
+    <section class="billboard_blue_wrapper offering_wrapper">
+        <div class="billboard center_text offering_billboard title_space title_nospace">
+            <div class="title_line">
+                <h2 class="offering_title_small">check out the</h2>
+            </div>
+            <h2 class="offering_title_big title_uppercase title_size_increase"><?php _e( 'Archives', 'html5blank' ); ?></h2>
+            <div class="billboard_para">
+                <p>Have fun perusing this selection of articles! I wrote each one with the intention of helping  you feel better in body, mind, and spirit.</p>
+            </div>
+        </div>
+    </section> <!-- closes .billboard_blue_wrapper -->
 
-			<h1><?php _e( 'Archives', 'html5blank' ); ?></h1>
+    <?php get_search_form(); ?>
 
-			<?php get_template_part('partials/loop'); ?>
+    <h2>Archives by Subject</h2>
+      <ul>
+        <?php wp_list_categories(); ?>
+      </ul>
 
-			<?php get_template_part('partials/pagination'); ?>
+    <h2>Archives by Month </h2>
+      <ul>
+        <?php wp_get_archives('type=monthly'); ?>
+      </ul>
 
-		</section>
-		<!-- /section -->
-	</main>
+    <!-- start blog preview -->
+    <section class="blog_preview">
+      <h2>And if you really have time to explore, scan these articles on diverse topics and see what interests you.</h2>
+            <!-- start blog card wrapper -->
+            <div class="blog-index-wrapper">
 
-<?php get_sidebar(); ?>
+    <?php if (have_posts()) :
+        while (have_posts()) :
+            the_post(); ?>
+
+                <!-- start individual blog preview card -->
+                    <div class="blog_card bottom_edge_shadow">
+                        <a href="<?php the_permalink() ?>">
+                        <div class="blog_card_details">
+                            <?php the_post_thumbnail(); ?>
+                            <h5><?php
+                                  // GET THE CATEGORY CONTENT
+                                  $categories = get_the_category ();
+
+                                  // IF THERE'S A CATEGORY
+                                  if ($categories) {
+                                  //LOOP THROUGH THE CATEGORIES AND DISPLAY THEM
+                                  $i = 1;
+                                  foreach ($categories as $one) {
+                                    echo $one->name;
+                                      // IF THERE ARE MORE THAN ONE CATEGORY
+                                      if (count($categories) > 0 && count($categories) > $i) {
+                                          echo ', ';
+                                      }
+                                      $i++;
+                                  }
+                                }
+                                ?>
+                            </h5>
+                            <h4><?php the_title() ?></h4>
+                            <p><?php the_excerpt() ?></p>
+                        </div>
+                        <div class="readtime">
+                            <p><?php echo do_shortcode('[rt_reading_time label="" postfix="min"]'); ?> read</p>
+                        </div>
+
+                        <div class="btn_round hvr-wobble-to-top-right">
+                            <a href="<?php the_permalink() ?>"><p >read</p></a>
+                        </div>
+                        </a>
+                    </div> <!-- end individual blog preview card -->
+
+        <?php endwhile; ?>
+        </div>  <!-- end blog card wrapper -->
+    </section>
+
+      <?php else: ?>
+        <em>No Posts Found</em>
+    <?php endif; ?>
+    <!--end blog preview -->
+
 
 <?php get_footer(); ?>
--- wp-content/themes/longlisa/tag.php
+++ wp-content/themes/longlisa/tag.php
@@ -1,19 +1,71 @@
 <?php get_header(); ?>
 
-	<main role="main">
-		<!-- section -->
-		<section>
+      <!-- BILL BOARD -->
+          <section class="billboard_blue_wrapper offering_wrapper">
+              <div class="billboard center_text offering_billboard title_space title_nospace">
+                  <div class="title_line">
+                      <h2 class="offering_title_small">learn about</h2>
+                  </div>
+                  <h2 class="offering_title_big title_uppercase title_size_increase"><?php _e( '', 'html5blank' ); single_tag_title('', true); ?></h2>
+                  <div class="billboard_para">
+                      <p>Dive deep into these articles that I wrote and curated especially for you to help you better understand the how, the why, and the when.</p>
+                  </div>
+              </div>
+          </section> <!-- closes .billboard_blue_wrapper -->
 
-			<h1><?php _e( 'Tag Archive: ', 'html5blank' ); echo single_tag_title('', false); ?></h1>
+          <!-- start blog preview -->
+          <section class="blog_preview">
+                  <!-- start blog card wrapper -->
+                  <div class="blog-index-wrapper">
 
-			<?php get_template_part('partials/loop'); ?>
+          <?php if (have_posts()) :
+              while (have_posts()) :
+                  the_post(); ?>
 
-			<?php get_template_part('partials/pagination'); ?>
+                      <!-- start individual blog preview card -->
+                          <div class="blog_card bottom_edge_shadow">
+                              <a href="<?php the_permalink() ?>">
+                              <div class="blog_card_details">
+                                  <?php the_post_thumbnail(); ?>
+                                  <h5><?php
+                                        // GET THE CATEGORY CONTENT
+                                        $categories = get_the_category ();
 
-		</section>
-		<!-- /section -->
-	</main>
+                                        // IF THERE'S A CATEGORY
+                                        if ($categories) {
+                                        //LOOP THROUGH THE CATEGORIES AND DISPLAY THEM
+                                        $i = 1;
+                                        foreach ($categories as $one) {
+                                          echo $one->name;
+                                            // IF THERE ARE MORE THAN ONE CATEGORY
+                                            if (count($categories) > 0 && count($categories) > $i) {
+                                                echo ', ';
+                                            }
+                                            $i++;
+                                        }
+                                      }
+                                      ?>
+                                  </h5>
+                                  <h4><?php the_title() ?></h4>
+                                  <p><?php the_excerpt() ?></p>
+                              </div>
+                              <div class="readtime">
+                                  <p><?php echo do_shortcode('[rt_reading_time label="" postfix="min"]'); ?> read</p>
+                              </div>
 
-<?php get_sidebar(); ?>
+                              <div class="btn_round hvr-wobble-to-top-right">
+                                  <a href="<?php the_permalink() ?>"><p >read</p></a>
+                              </div>
+                              </a>
+                          </div> <!-- end individual blog preview card -->
+
+              <?php endwhile; ?>
+              </div>  <!-- end blog card wrapper -->
+          </section>
+
+            <?php else: ?>
+              <em>No Posts Found</em>
+          <?php endif; ?>
+          <!--end blog preview -->
 
 <?php get_footer(); ?>
