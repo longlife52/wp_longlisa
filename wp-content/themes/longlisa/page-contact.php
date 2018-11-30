9714ceb1a7e4a3adc5a6819ef8592665d45add5b
Lisa Long <garrisonridge@gmail.com>
2018-11-29 20:37:46 -0500

    Add partials to contact page

--- wp-content/themes/longlisa/page-contact.php
+++ wp-content/themes/longlisa/page-contact.php
@@ -24,101 +24,16 @@ get_header(); ?>
           <?php get_template_part('partials/dots'); ?>
         </div>
 
-        <!-- start FLIP CARDS -->
-                <section class="flex_flip_container">
-                  <!-- open flex_item ONE -->
-                    <div class="flex_item flip_one">
-                    	<div class="flex_item_inner">
-                        	<!-- card -->
-                        	<a href="singlepost.html">
-                                <div class="card-front bg-bright">
-                                  <div class="flip_img">
-                                    <img src="<?php bloginfo('template_url'); ?>/assets/dist/img/brain.png">
-                                  </div>
-                                    <h4>Improve Posture</h4>
-                                    <p class="detail">today</p>
-                                </div>
-                                <div class="card-back bg-bright">
-                                    <p class="title">Lorem ipsum dolor sit amet</p>
-                                    <p class="desc">Pellentesque magna nunc, fermentum nec ipsum non, </p>
-                                    <p class="link">Details</p>
-                                </div>
-                            </a>
-                            <!-- /card -->
-                        </div> <!-- close flex_item_inner ONE -->
-                    </div> <!-- close flex_item ONE -->
+<?php get_template_part('partials/flip_card'); ?>
 
-                    <!-- open flex_item TWO -->
-                      <div class="flex_item flip_two">
-                      	<div class="flex_item_inner">
-                          	<!-- card -->
-                          	<a href="singlepost.html">
-                                  <div class="card-front bg-dark">
-                                    <div class="flip_img">
-                                      <img src="<?php bloginfo('template_url'); ?>/assets/dist/img/power.png">
-                                    </div>
-                                      <h4>Sleep Better</h4>
-                                      <p class="detail">tonight</p>
-                                  </div>
-                                  <div class="card-back bg-dark">
-                                      <p class="title">Lorem ipsum dolor sit amet</p>
-                                      <p class="desc">Pellentesque magna nunc</p>
-                                      <p class="link">Details</p>
-                                  </div>
-                              </a>
-                              <!-- /card -->
-                          </div> <!-- close flex_item_inner TWO -->
-                      </div> <!-- close flex_item TWO -->
+<div class="dots_hug_up dots_space_bottom">
+  <?php get_template_part('partials/dots'); ?>
+</div>
 
-                      <!-- open flex_item THREE -->
-                        <div class="flex_item flip_three">
-                        	<div class="flex_item_inner">
-                            	<!-- card -->
-                            	<a href="singlepost.html">
-                                    <div class="card-front bg-blue">
-                                      <div class="flip_img">
-                                        <img src="<?php bloginfo('template_url'); ?>/assets/dist/img/knee.png">
-                                      </div>
-                                        <h4>Increase Vitality</h4>
-                                        <p class="detail">now</p>
-                                    </div>
-                                    <div class="card-back bg-blue">
-                                        <p class="title">Lorem ipsum dolor sit amet</p>
-                                        <p class="desc">Pellentesque magna nunc, fermentum nec ipsum non, consequat scelerisque dui.</p>
-                                        <p class="link">Details</p>
-                                    </div>
-                                </a>
-                                <!-- /card -->
-                            </div> <!-- close flex_item_inner THREE -->
-                        </div> <!-- close flex_item THREE -->
-
-                        <!-- open flex_item FOUR -->
-                          <div class="flex_item flip_four">
-                          	<div class="flex_item_inner">
-                              	<!-- card -->
-                              	<a href="singlepost.html">
-                                      <div class="card-front bg-bright">
-                                        <div class="flip_img">
-                                          <img src="<?php bloginfo('template_url'); ?>/assets/dist/img/power.png">
-                                        </div>
-                                          <h4>Relieve Stress</h4>
-                                          <p class="detail">fast</p>
-                                      </div>
-                                      <div class="card-back bg-bright">
-                                          <p class="title">Lorem ipsum dolor sit amet</p>
-                                          <p class="desc">Pellentesque magna nunc, fermentum nec ipsum non, consequat scelerisque dui.</p>
-                                          <p class="link"> Details</p>
-                                      </div>
-                                  </a>
-                                  <!-- /card -->
-                              </div> <!-- close flex_item_inner FOUR -->
-                          </div> <!-- close flex_item FOUR -->
-                </section>
-        <!-- end FLIP CARDS -->
-
-        <div class="dots_hug_up dots_space_bottom">
-          <?php get_template_part('partials/dots'); ?>
-        </div>
-
+<!-- start 3 ARTICLE ICON/class cards -->
+<div class="contact_reduce_space">
+<?php get_template_part('partials/article_icon'); ?>
+<!-- end 3 ARTICLE ICON/class cards -->
+</div>
 
 <?php get_footer(); ?>
