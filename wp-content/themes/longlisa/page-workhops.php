5d5ce18d33e7491754ff5b8ee0f39e3be9c36ea8
Lisa Long <garrisonridge@gmail.com>
2018-12-13 13:54:47 -0500

    Add partials to online page

--- wp-content/themes/longlisa/page-workhops.php
+++ wp-content/themes/longlisa/page-workhops.php
@@ -8,5 +8,13 @@ get_header(); ?>
 
 <?php get_template_part('partials/pix_header'); ?>
 
+<!-- start TESTIMONIAL TWO BLOCK  -->
+<?php get_template_part('partials/testimonial_two_block'); ?>
+<!-- end TESTIMONIAL TWO BLOCK  -->
+
+<!-- start BLOG CATEGORIES 3 ICON/class cards -->
+<?php get_template_part('partials/category_preview'); ?>
+<!-- end BLOG CATEGORIES 3 ICON/class cards -->
+
 
 <?php get_footer(); ?>
